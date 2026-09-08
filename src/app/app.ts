e29e0c068300293290ab994f97e26f39180b9b5a
gabriel farias ferreira <gabrielfariasferreira2008@gmail.com>
2026-09-08 13:54:59 -0300

    feat atualizando o css html e o ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
